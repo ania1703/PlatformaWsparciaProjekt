0b1521b871d0dbcc6b99ff167f1f70b317002dea
ania1703 <ania170304@gmail.com>
2025-03-25 21:22:29 +0100

    Utworzenie modeli dla seniora (Senior.cs), zgłoszenia (Request.cs) i wolontariusza (Volunteer.cs). Dodanie klasy AppDbContext.

---
+++ PlatformaWsparciaProjekt/Models/Request.cs
@@ -0,0 +1,18 @@
+﻿namespace PlatformaWsparciaProjekt.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsCompleted { get; set; }
+
+        // Relacja ManyToOne - jedno zgłoszenie przypisane do jednego seniora
+        public int SeniorId { get; set; }
+        public Senior Senior { get; set; }
+
+        // Relacja ManyToOne - jedno zgłoszenie może być przypisane do jednego wolontariusza
+        public int? VolunteerId { get; set; }
+        public Volunteer? Volunteer { get; set; }
+    }
+}
---
+++ PlatformaWsparciaProjekt/Models/Volunteer.cs
@@ -0,0 +1,14 @@
+﻿namespace PlatformaWsparciaProjekt.Models
+{
+    public class Volunteer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        // Relacja OneToMany - Wolontariusz może realizować wiele zgłoszeń
+        public List<Request> AssignedRequests { get; set; } = new List<Request>();
+    }
+}
