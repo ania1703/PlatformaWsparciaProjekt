f6dfd11143faa33a081f862a784cdd5799f00340
Sofiia Hurbanova <sonagurbanova3@gmail.com>
2025-03-25 23:25:27 +0100

    sofia//dodanie klas

---
+++ PlatformaWsparciaProjekt/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿namespace PlatformaWsparciaProjekt.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int RatedById { get; set; }
+        public User RatedBy { get; set; }
+
+        public double Score { get; set; } // Ocena w skali np. 1-5
+        public string Comment { get; set; }
+        public DateTime RatedAt { get; set; } = DateTime.Now;
+    }
+}
