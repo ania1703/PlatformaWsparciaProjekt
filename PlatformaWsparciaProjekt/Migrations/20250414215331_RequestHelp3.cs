cc6b53883e7d05ceb957648d16129636d5be1fb3
emilkawalczyk <emilkawalczyk@wp.pl>
2025-04-15 00:22:58 +0200

    Dodanie możliwości tworzenia próśb wyłącznie dla zalogowanych seniorór, dodanie możliwości wybory prośby przez wolontariusza

---
+++ PlatformaWsparciaProjekt/Migrations/20250414215331_RequestHelp3.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlatformaWsparciaProjekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class RequestHelp3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VolunteerId",
+                table: "HelpRequests",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HelpRequests_VolunteerId",
+                table: "HelpRequests",
+                column: "VolunteerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HelpRequests_Volunteers_VolunteerId",
+                table: "HelpRequests",
+                column: "VolunteerId",
+                principalTable: "Volunteers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HelpRequests_Volunteers_VolunteerId",
+                table: "HelpRequests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HelpRequests_VolunteerId",
+                table: "HelpRequests");
+
+            migrationBuilder.DropColumn(
+                name: "VolunteerId",
+                table: "HelpRequests");
+        }
+    }
+}
